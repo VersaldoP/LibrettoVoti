557fa129d63e3dddbd6206e0af082a1200083b95
pietr <pietr@LAPTOP-FCSJ88N2>
2021-03-08 12:24:08 +0100

    soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -3,37 +3,19 @@ package it.polito.tdp.librettovoti.model;
 import java.time.LocalDate;
 
 public class TestLibretto {
-	private String nome;
-	private int voto;
-	private LocalDate data;
-	public TestLibretto(String nome, int voto, LocalDate data) {
-		super();
-		this.nome = nome;
-		this.voto = voto;
-		this.data = data;
+	public static void main(String args[]) {
+		
+		
+		
+		Libretto libretto= new Libretto();
+		
+		Voto voto1 = new Voto("Analisi 1",30,LocalDate.of(2019, 2, 15));
+		
+		libretto.add(voto1);
+		libretto.add(new Voto("Fisica 1",27,LocalDate.of(2019, 7, 23)));
+		libretto.add(new Voto("Chimica 1",25,LocalDate.of(2019, 2, 11)));
+		libretto.add(new Voto("Fisica 1",18,LocalDate.of(2019, 9, 1)));
+		System.out.println(libretto.toString());
 	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getVoto() {
-		return voto;
-	}
-	public void setVoto(int voto) {
-		this.voto = voto;
-	}
-	public LocalDate getData() {
-		return data;
-	}
-	public void setData(LocalDate data) {
-		this.data = data;
-	}
-	@Override
-	public String toString() {
-		return "Esame=" + nome + " superato con" + voto + " il" + data;
-	}
-	
 
 }
